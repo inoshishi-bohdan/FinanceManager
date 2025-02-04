db509084743b3455fe937eda2727b617e4121aac
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-04 21:44:25 +0100

    fix: remove router action

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,20 +1,65 @@
-import { Form, useActionData, useNavigation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import ErrorBox from "./ErrorBox";
 import Input from "./Input";
+import { useState } from "react";
 
 export default function RegisterForm() {
-   const data = useActionData();
-   const navigation = useNavigation();
-   const isSubmitting = navigation.state === 'submitting'
+   const navigate = useNavigate();
+   const [isSubmitting, setIsSubmitting] = useState(false); 
+   const [responseMessage, setResponseMessage] = useState(null);
+
+   async function handleSubmit(event) {
+      event.preventDefault();
+      setIsSubmitting(true); 
+ 
+      const fd = new FormData(event.target);
+      const data = Object.fromEntries(fd.entries());
+      const request = {
+         userName: data.userName,
+         email: data.email,
+         password: data.password
+      };
+      let response;
+
+      try {
+         response = await fetch(`https://localhost:7209/api/Auth/register`, {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(request)
+         });
+      } catch (error) {
+         console.error("Fetch Error:", error);
+         navigate('/error', { state: { message: "A network error occurred", status: 500 } }); // Include status
+         return;
+      } finally { 
+         setIsSubmitting(false);
+     }
+
+      if (response.status === 400) {
+         const errorData = await response.json();
+         setResponseMessage(errorData);
+         return;
+      }
+
+      if (!response.ok) {
+         navigate('/error', { state: { message: "Could not register user", status: 500 } });
+         return;
+      }
+      navigate('/login');
+   }
 
    return (
-      <Form className='auth' method='POST'>
-         <h4 className='text-center auth mb-5'>Sign Up</h4>
-         {data && data.message && data.errors && <ErrorBox message={data.message} errors={data.errors} />}
+      <form className='auth' onSubmit={handleSubmit}>
+         <h4 className='text-center auth mb-4'>Sign Up</h4>
+         {responseMessage && responseMessage.message && <ErrorBox message={responseMessage.message} errors={responseMessage.errors} />}
          <Input required type="text" label='User name' name='userName' id='userName' />
          <Input required type="email" label='Email address' name='email' id='email' />
          <Input required type="password" label='Password' name='password' id='password' />
-         <button type="submit" className={`btn btn-primary auth mt-4`}>{isSubmitting ? 'Submitting...' : 'Sign Up'}</button>
-      </Form>
+         <button type="submit" className={`btn btn-primary auth mt-4`} disabled={isSubmitting}>
+            {isSubmitting ? 'Submitting...' : 'Sign Up'}
+         </button>
+      </form>
    );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,3 @@
-import PageContent from "../components/PageContent";
 import RegisterForm from "../components/RegisterForm";
 import { redirect } from 'react-router-dom';
 
@@ -9,19 +8,25 @@ export default function RegisterPage() {
 
 export async function action({ request }) {
    const data = await request.formData();
-   const authData = {
+   const requestBody = {
       userName: data.get('userName'),
       email: data.get('email'),
       password: data.get('password')
    }
+   let response;
 
-   const response = await fetch(`https://localhost:7209/api/Auth/register`, {
-      method: 'POST',
-      headers: {
-         'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(authData)
-   });
+   try {
+      response = await fetch(`https://localhost:7209/api/Auth/register`, {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(requestBody)
+      });
+   } catch (error) {
+      console.error("Fetch Error:", error);
+      throw new Response(JSON.stringify({ message: 'A network error occurred' }, { status: 500 })); 
+   }
 
    if (response.status === 400) {
       return response;
