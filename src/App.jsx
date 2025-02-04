409f8d32e867d4305090149adb0d26c38b213a67
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-04 21:47:04 +0100

    added: AuthContext

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,48 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import RootLayout from './pages/Root';
 import LoginPage from './pages/Login';
 import RegisterPage from './pages/Register';
-import ErrorPage from './pages/Error';
+import RouterErrorPage from './pages/RouterError';
 import HomePage from './pages/Home';
 import IncomePage from './pages/Incomes';
 import ExpensesPage from './pages/Expenses';
 import StatisticsPage from './pages/Statistics';
-import { action as registerAction } from './pages/Register';
-import { action as loginAction } from './pages/Login';
+import { createContext, useState } from 'react';
+import NavigateErrorPage from './pages/NavigateError';
+
+
+export const AuthContext = createContext();
 
 const router = createBrowserRouter([
    {
-     path: '/',
-     element: <RootLayout />,
-     errorElement: <ErrorPage />,
-     children: [
-       { index: true, element: <HomePage /> },  
-       {path: 'incomes', element: <IncomePage />},
-       {path: 'expenses', element: <ExpensesPage />}    ,
-       {path: 'statistics', element: <StatisticsPage />},
-       {path: 'login', element: <LoginPage />, action: loginAction},
-       {path: 'register', element: <RegisterPage />, action: registerAction} 
-     ]
+      path: '/',
+      element: <RootLayout />,
+      errorElement: <RouterErrorPage />,
+      children: [
+         { index: true, element: <HomePage /> },
+         { path: 'incomes', element: <IncomePage /> },
+         { path: 'expenses', element: <ExpensesPage /> },
+         { path: 'statistics', element: <StatisticsPage /> },
+         { path: 'login', element: <LoginPage /> },
+         { path: 'register', element: <RegisterPage /> },
+         { path: 'error', element: <NavigateErrorPage /> }
+      ]
    }
- ]);
-
+]);
 
 function App() {
-   return <RouterProvider router={router} />;
+   const [isAuthenticated, setIsAuthenticated] = useState(localStorage.accessToken ? true : false);
+
+   function changeIsAuthenticated(value) {
+      setIsAuthenticated(value);
+
+      if (value === false) {
+         localStorage.clear();
+      }
+   }
+
+   return <AuthContext.Provider value={[isAuthenticated, changeIsAuthenticated]}>
+      <RouterProvider router={router} />;
+   </AuthContext.Provider>
 }
 
 export default App
