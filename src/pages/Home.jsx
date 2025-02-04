9f760153a5392e6245a3d00bca3cac721845464e
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-04 21:46:49 +0100

    fix: structure update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import PageContent from "../components/PageContent";
 
 export default function HomePage () {
-   return <PageContent title='Welcome' subtitle='This is a finance manager!'>
-      
-   </PageContent>
+   return <PageContent title='Welcome' subtitle='This is a finance manager!' />;
 }
