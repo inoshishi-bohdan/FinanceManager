96acc94db0f8c170723c20b2c16177882532c76e
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-06 21:21:07 +0100

    fix: missing import

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../store/authentication-context";
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 export default function ProtectedRoute() {
    const { isAuthenticated } = useContext(AuthContext);
