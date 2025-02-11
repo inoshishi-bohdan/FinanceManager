f20d7c59e0e34d7844e6f0c394fc17b6e471cfaf
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-11 20:39:06 +0100

    fix: RowActions update

--- src/components/RowActions.jsx
+++ src/components/RowActions.jsx
@@ -1,8 +1,8 @@
-export default function RowActions({row}) {
+export default function RowActions({onEditClick, onDeleteClick}) {
    return (
       <div className="d-flex flex-wrap gap-2">
-         <button type="button" className="btn btn-outline-warning action-button" onClick={() => console.log(row.original)} >Edit</button> 
-         <button type="button" className="btn btn-outline-danger action-button" onClick={() => console.log(row.original)} >Delete</button> 
+         <button type="button" className="btn btn-outline-warning action-button" onClick={onEditClick} >Edit</button>
+         <button type="button" className="btn btn-outline-danger action-button" onClick={onDeleteClick} >Delete</button>
       </div>
    );
 }
