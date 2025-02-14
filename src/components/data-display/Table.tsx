21b734581d18cb3de6be1aef8e13f6ab90da5476
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-14 10:18:48 +0100

    fix: reusable Table

--- src/components/data-display/Table.tsx
+++ src/components/data-display/Table.tsx
@@ -4,16 +4,47 @@ import {
 } from '@tanstack/react-table'
 import TablePagination from './TablePagination';
 import TableHeader from './TableHeader';
+import {
+   ColumnFiltersState,
+   getCoreRowModel,
+   getFacetedMinMaxValues,
+   getFacetedRowModel,
+   getFacetedUniqueValues,
+   getFilteredRowModel,
+   getPaginationRowModel,
+   getSortedRowModel,
+   useReactTable,
+} from '@tanstack/react-table'
 
-export default function Table({ tableConfiguration, name }) {
+export default function Table({ data, columns, name }) {
+   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
+   const config = useReactTable({
+      data,
+      columns,
+      state: {
+         columnFilters,
+      },
+      onColumnFiltersChange: setColumnFilters,
+      getCoreRowModel: getCoreRowModel(),
+      getFilteredRowModel: getFilteredRowModel(), //client-side filtering
+      getSortedRowModel: getSortedRowModel(),
+      getPaginationRowModel: getPaginationRowModel(),
+      getFacetedRowModel: getFacetedRowModel(), // client-side faceting
+      getFacetedUniqueValues: getFacetedUniqueValues(), // generate unique values for select filter/autocomplete
+      getFacetedMinMaxValues: getFacetedMinMaxValues(), // generate min/max values for range filter
+      debugTable: true,
+      debugHeaders: true,
+      debugColumns: false,
+   })
+
    return (
       <div className='table-container'>
          <h1 className='table-name'>{name}</h1>
          <div className='table-responsive'>
             <table className="table table-hover table-bordered">
-               <TableHeader tableConfiguration={tableConfiguration} />
+               <TableHeader tableConfiguration={config} />
                <tbody>
-                  {tableConfiguration.getRowModel().rows.map(row => {
+                  {config.getRowModel().rows.map(row => {
                      return (
                         <tr key={row.id}>
                            {row.getVisibleCells().map(cell => {
@@ -32,7 +63,7 @@ export default function Table({ tableConfiguration, name }) {
                </tbody>
             </table>
          </div>
-         <TablePagination tableConfiguration={tableConfiguration} />
+         <TablePagination tableConfiguration={config} />
       </div>
    );
 }
