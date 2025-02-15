028247d5480bf25960e07da5bddda3ffd4e0e6e2
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-15 22:29:34 +0100

    fix: removed EditIncomeForm.jsx

--- src/components/ui/EditIncomeForm.jsx
+++
@@ -1,9 +0,0 @@
-import Modal from "./Modal";
-
-export default function EditIncomeForm({ isOpen, record, onClose }) {
-   return (
-      <Modal open={isOpen} className="edit-form" onClose={onClose}>
-         <p>Edit form</p>
-      </Modal>
-   );
-}
