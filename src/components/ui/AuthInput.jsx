4380c84beb5ca31901ed4b00de250e5a48cf7d4e
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-15 22:26:59 +0100

    added: AuthInput.jsx

---
+++ src/components/ui/AuthInput.jsx
@@ -0,0 +1,9 @@
+export default function AuthInput({ label, id, error, ...props }) {
+   return (
+      <div className='mb-3'>
+         <label htmlFor={id} className="form-label auth">{label}</label>
+         <input {...props} className={`form-control auth`} id={id} />
+      </div>
+   );
+}
+
