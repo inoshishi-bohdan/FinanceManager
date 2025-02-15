ba34115a8de0704f216b3f0f0bc6dd33c578a429
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-15 22:29:17 +0100

    added: IncomeRemover.jsx

---
+++ src/components/ui/IncomeRemover.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { IncomeModalContext } from "../../store/income-modal-context";
+import { queryClient } from "../../util/http";
+import { showSuccessNotification } from "../../util/notification";
+import DeleteConfirmation from "./DeleteConfirmation";
+import { deleteIncome } from "../../util/http";
+import Modal from "./Modal";
+
+export default function IncomeRemover() {
+      const {deleteModalIsOpen, handleCloseDeleteModal, selectedRecord, setSelectedRecord} = useContext(IncomeModalContext);
+   
+      function handleSuccessDelete() {
+         queryClient.invalidateQueries({ queryKey: ['incomes'] });
+         showSuccessNotification('Record was successfully deleted');
+         handleCloseDeleteModal();
+         setSelectedRecord(null);
+      }
+
+   return (
+      <Modal open={deleteModalIsOpen} onClose={handleCloseDeleteModal}>
+         <DeleteConfirmation record={selectedRecord} mutateFn={deleteIncome} onSuccess={handleSuccessDelete} onCancel={handleCloseDeleteModal}  />
+      </Modal>
+   );
+}
