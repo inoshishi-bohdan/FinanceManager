38a99728b50a10dcdab690c6f8cda2c297a24148
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-15 22:25:36 +0100

    added: Select.jsx

---
+++ src/components/ui/Select.jsx
@@ -0,0 +1,13 @@
+export default function Select({ text, label, id, options, ...props }) {
+   return (
+      <div className="mb-2">
+         <label htmlFor={id} className="form-label">{label}</label>
+         <select {...props} id={id} className="form-select" aria-label="Default select example" >
+            <option value='' disabled>{text}</option>
+            {options.map(option => <option key={option.id} value={option.id}>
+               {option.name}
+            </option>)}
+         </select>
+      </div>
+   );
+}
