0d4e2b565038fcaeca66570167d2de5c48b710e0
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-16 17:50:41 +0100

    fix: ExpenseTable in Expenses.jsx

--- src/pages/Expenses.jsx
+++ src/pages/Expenses.jsx
@@ -1,5 +1,24 @@
-import PageContent from "../components/layout/PageContent";
+import LoadingIndicator from "../components/UI/LoadingIndicator";
+import { fetchExpenses } from "../util/http";
+import useMyQuery from "../hooks/useMyQuery";
+import ExpenseModalContextProvider from "../store/expense-modal-context";
+import ExpenseTable from "../components/data-display/ExpenseTable";
 
 export default function ExpensePage() {
-   return <PageContent title='Expenses'/>
+   const { isPending, data } = useMyQuery(fetchExpenses, ['expenses']);
+   let content;
+
+   if (isPending) {
+      content = <LoadingIndicator />
+   }
+
+   if (data) {
+      content = (
+         <ExpenseModalContextProvider>
+            <ExpenseTable data={data} />
+         </ExpenseModalContextProvider>
+      );
+   }
+
+   return content;
 }
