bb4d4a7dc18a0730bc0a7acc1bc2ad15a76a4f03
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-21 11:24:12 +0100

    added: LineChart.jsx

---
+++ src/components/data-display/LineChart.jsx
@@ -0,0 +1,37 @@
+import {
+   VictoryChart,
+   VictoryLine,
+   VictoryTheme,
+   VictoryTooltip,
+   VictoryVoronoiContainer
+} from "victory";
+import LoadingIndicator from "../ui/LoadingIndicator";
+
+export default function LineChart({ chartData, isLoadingChartData }) {
+   return <div className="chart-body">
+      {isLoadingChartData && <LoadingIndicator />}
+      {chartData && <>
+         <VictoryChart
+            domainPadding={{ x: 15, y: 5 }}
+            theme={VictoryTheme.clean}
+            containerComponent={
+               <VictoryVoronoiContainer
+                  voronoiDimension="x"
+                  labels={({ datum }) =>
+                     `total : ${datum.y}`
+                  }
+                  labelComponent={
+                     <VictoryTooltip />
+                  }
+               />
+            }
+         >
+            <VictoryLine
+               data={chartData.map((d) => ({ x: d.month, y: d.totalAmount }))}
+               interpolation='natural'
+               animate={{ duration: 1000 }}
+            />
+         </VictoryChart>
+      </>}
+   </div>;
+}
