457c268b36f5bd2fc6c21a6b5c6e50b8ad7902cb
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-21 09:53:39 +0100

    added: useChart.js

---
+++ src/hooks/useChart.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { fetchCurrencies } from '../util/http';
+import useMyQuery from "./useMyQuery";
+
+export default function useChart({ chartQueryFn, chartQueryKey, recordPeriodQueryFn, recordPeriodQueryKey }) {
+   const [selectedYear, setSelectedYear] = useState(null);
+   const [selectedCurrency, setSelectedCurrency] = useState(null);
+
+   const { isPending: isPendingRecordPeriod, data: recordPeriod } = useMyQuery(recordPeriodQueryFn, recordPeriodQueryKey);
+   const { data: currencies, isPending: isPendingCurrencies } = useMyQuery(fetchCurrencies, ['currencies']);
+   const { isLoading: isLoadingChartData, data: chartData } = useMyQuery(
+      ({ signal }) => chartQueryFn({
+         signal: signal,
+         request: { currencyId: selectedCurrency, year: selectedYear }
+      }),
+      [...chartQueryKey, { currencyId: selectedCurrency, year: selectedYear }],
+      !!(selectedYear && selectedCurrency));
+
+   useEffect(() => {
+      if (recordPeriod && recordPeriod.length > 0) {
+         const latestYear = recordPeriod[recordPeriod.length - 1];
+         setSelectedYear(latestYear);
+      }
+   }, [recordPeriod]);
+
+   useEffect(() => {
+      if (currencies && currencies.length > 0) {
+         setSelectedCurrency(currencies[0].id);
+      }
+   }, [currencies]);
+
+   function handleSelectedYearChange(event) {
+      const year = Number(event.target.value)
+      setSelectedYear(year);
+   }
+
+   function handleSelectedCurrencyChange(event) {
+      const currencyId = Number(event.target.value)
+      setSelectedCurrency(currencyId);
+   }
+
+   return ({
+      chartData,
+      recordPeriod,
+      currencies,
+      selectedCurrency,
+      selectedYear,
+      isLoadingChartData,
+      isPendingCurrencies,
+      isPendingRecordPeriod,
+      handleSelectedCurrencyChange,
+      handleSelectedYearChange
+   });
+}
