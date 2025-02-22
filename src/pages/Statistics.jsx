b1cbf42234a222a9fa19221759d11497f3fef396
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-22 17:42:09 +0100

    fix: Statistics page structure changes

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,5 +1,17 @@
-import PageContent from "../components/layout/PageContent";
+import IncomeChart from "../components/data-display/IncomeChart";
+import ExpenseChart from "../components/data-display/ExpenseChart";
+import NetWorthChart from "../components/data-display/NetWorthChart";
+import IncomeDistributionChart from "../components/data-display/IncomeDistributionChart";
+import ExpenseDistributionChart from "../components/data-display/ExpenseDistributionChart";
 
 export default function StatisticPage() {
-   return <PageContent title='Statistics' />;
+   return (
+      <div className="chart-container">
+         <IncomeChart />
+         <IncomeDistributionChart />
+         <ExpenseChart />
+         <ExpenseDistributionChart />
+         <NetWorthChart />
+      </div>
+   );
 }
