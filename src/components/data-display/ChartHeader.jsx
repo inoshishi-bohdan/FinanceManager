110039c8deb5794722d6cd448def24c5783ef4a4
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-22 17:40:48 +0100

    fix: null value handling

--- src/components/data-display/ChartHeader.jsx
+++ src/components/data-display/ChartHeader.jsx
@@ -1,17 +1,16 @@
-
 export default function ChartHeader({ title, selectedYear, selectedCurrency, recordPeriod, currencies, onCurrencyChange, onYearChange }) {
    return (
       <div className='chart-header'>
          <p className="fs-2 fw-semibold m-0">{title}</p>
          <div className="d-flex gap-3">
-            <select onChange={onYearChange} className="form-select" value={selectedYear} >
-               <option disabled>Please select year</option>
+            <select onChange={onYearChange} className="form-select" value={selectedYear ?? ''} >
+               <option value='' disabled>Please select year</option>
                {recordPeriod.map(year => <option key={year} value={year}>
                   {year}
                </option>)}
             </select>
-            <select onChange={onCurrencyChange} className="form-select" value={selectedCurrency} >
-               <option disabled>Please select currency</option>
+            <select onChange={onCurrencyChange} className="form-select" value={selectedCurrency ?? ''} >
+               <option value='' disabled>Please select currency</option>
                {currencies.map(currency => <option key={currency.id} value={currency.id}>
                   {currency.name}
                </option>)}
