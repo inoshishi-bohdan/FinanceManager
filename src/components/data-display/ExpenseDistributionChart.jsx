eaf3a023eefd952eee471f0919e4d57579b9b745
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-22 17:41:47 +0100

    added: ExpenseDistributionChart.jsx

---
+++ src/components/data-display/ExpenseDistributionChart.jsx
@@ -0,0 +1,50 @@
+import PieChart from './PieChart'
+import { fetchExpenseDistribution, fetchExpenseRecordPeriod } from '../../util/http';
+import LoadingIndicator from '../ui/LoadingIndicator';
+import useDistributionChart from '../../hooks/useDistributionChart';
+import DistributionChartHeader from './DistributionChartHeader'
+
+export default function ExpenseDistributionChart() {
+   const {
+      chartData,
+      recordPeriod: expenseRecordPeriod,
+      selectedMonth,
+      selectedYear,
+      isLoadingChartData,
+      isPendingRecordPeriod: isPendingExpenseRecordPeriod,
+      handleSelectedMonthChange,
+      handleSelectedYearChange
+   } = useDistributionChart({
+      chartQueryFn: fetchExpenseDistribution,
+      chartQueryKey: ['expenses, expense-distribution-statistic'],
+      recordPeriodQueryFn: fetchExpenseRecordPeriod,
+      recordPeriodQueryKey: ['expenses, expense-record-period']
+   });
+
+   let content;
+
+   if (isPendingExpenseRecordPeriod) {
+      content = <LoadingIndicator />
+   }
+
+   if (expenseRecordPeriod) {
+      content = <>
+         <DistributionChartHeader
+            title='Expense Distribution'
+            selectedYear={selectedYear}
+            selectedMonth={selectedMonth}
+            recordPeriod={expenseRecordPeriod}
+            onMonthChange={handleSelectedMonthChange}
+            onYearChange={handleSelectedYearChange}
+         />
+         <PieChart isLoadingChartData={isLoadingChartData} chartData={chartData} />
+      </>
+   }
+
+   return (
+      <div className='chart'>
+         {content}
+      </div>
+   );
+
+}
