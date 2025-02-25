f9de5d77a848eb636c222a7a2f418dc486123f31
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-25 11:03:18 +0100

    added: UserRemover.jsx

---
+++ src/components/ui/UserRemover.jsx
@@ -0,0 +1,28 @@
+import { showInfoNotification } from "../../util/notification";
+import DeleteConfirmation from "./DeleteConfirmation";
+import { deleteMyAccount } from "../../util/http";
+import Modal from "./Modal";
+import { UserModalContext } from "../../store/user-modal-context";
+import { useContext } from 'react'
+import { AuthContext } from "../../store/authentication-context";
+import { useNavigate } from "react-router-dom";
+
+export default function UserRemover() {
+   const { deleteModalIsOpen, handleCloseDeleteModal } = useContext(UserModalContext);
+   const { changeIsAuthenticated } = useContext(AuthContext);
+   const navigate = useNavigate();
+
+   function handleSuccessDelete() {
+      queryClient.invalidateQueries();
+      showInfoNotification('Your account was successfully deleted');
+      handleCloseDeleteModal();
+      changeIsAuthenticated(false);
+      navigate('/login');
+   }
+
+   return (
+      <Modal open={deleteModalIsOpen} onClose={handleCloseDeleteModal}>
+         <DeleteConfirmation text='Do you want to permanently delete your account?' mutateFn={deleteMyAccount} onSuccess={handleSuccessDelete} onCancel={handleCloseDeleteModal} />
+      </Modal>
+   );
+}
