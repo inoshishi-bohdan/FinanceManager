9c8c97798bef28683d75fd50d47ea451b545dbf2
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-25 11:02:37 +0100

    fix: usage UserModalContextProvider in Settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,10 @@
 import UserCard from "../components/ui/UserCard";
+import UserModalContextProvider from "../store/user-modal-context";
 
 export default function SettingPage() {
    return (
-      <UserCard />
+      <UserModalContextProvider>
+         <UserCard />
+      </UserModalContextProvider>
    );
 }
