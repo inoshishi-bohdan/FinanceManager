4fabbad55c837485ea6a92ebc4cd909613985c08
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-26 23:42:09 +0100

    fix: MONTH array

--- src/util/mapping.js
+++ src/util/mapping.js
@@ -1,4 +1,4 @@
-export const MONTHS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+export const MONTHS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 export function monthNumberToString(monthNumber) {
    if (typeof monthNumber !== 'number' || monthNumber < 1 || monthNumber > 12) {
