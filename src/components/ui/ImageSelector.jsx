35b22451f834500d13e9486669dbf2e1c2ae7b16
Bohdan Koba <bohdan.koba@dxc.com>
2025-03-16 15:44:48 +0100

    fix: framer-motion in ImageSelector.jsx

--- src/components/ui/ImageSelector.jsx
+++ src/components/ui/ImageSelector.jsx
@@ -2,14 +2,21 @@ import useMyQuery from '../../hooks/useMyQuery';
 import { BASE_URL, fetchProfileImages } from '../../util/http';
 import LoadingIndicator from './LoadingIndicator';
 import classes from './ImageSelector.module.css'
+import { motion } from 'framer-motion';
+
 
 export default function ImageSelector({ selectedImageId, onImageOptionClick }) {
    const { data: availableProfileImages, isPending: isPendingAvailableProfileImages } = useMyQuery(fetchProfileImages, ['available-profile-images']);
 
    return (
-      <div className={classes['image-selector']}>
+      <div
+         transition={{ staggerChildren: 0.05 }}
+         className={classes['image-selector']}>
          {isPendingAvailableProfileImages && <LoadingIndicator />}
-         {availableProfileImages && availableProfileImages.map(image => <img
+         {availableProfileImages && availableProfileImages.map(image => <motion.img
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: [0.8, 1] }}
+            exit={{ opacity: 1, scale: 1 }}
             className={`${classes['image-option']} ${selectedImageId === image.id ? classes['selected'] : ''}`}
             onClick={() => onImageOptionClick(image.id)}
             key={image.path}
