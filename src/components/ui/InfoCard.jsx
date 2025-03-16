5e616fffe3d1031b5cf3f3b75d1530102f3d0570
Bohdan Koba <bohdan.koba@dxc.com>
2025-03-16 16:01:42 +0100

    fix: framer-motion InfoCard.jsx

--- src/components/ui/InfoCard.jsx
+++ src/components/ui/InfoCard.jsx
@@ -1,10 +1,16 @@
+import { motion } from "framer-motion"
+
 export default function InfoCard({ src, alt, text }) {
    return (
-      <div className="card info-card" >
+      <motion.div
+         initial={{ y: 30, opacity: 0 }}
+         animate={{ y: 0, opacity: 1 }}
+         transition={{ duration: 0.5 }}
+         className="card info-card" >
          <img src={src} className="card-img-top" alt={alt} />
          <div className="card-body">
             <p className="card-text">{text}</p>
          </div>
-      </div>
+      </motion.div>
    )
 }
